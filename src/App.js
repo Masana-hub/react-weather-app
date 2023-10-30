20b812f9f5ae94c3f7cc3efde4610735aff537ca
Masana-hub <nwagezani.mabasa@gmail.com>
2023-10-30 10:49:36 +0200

    added bootstrap and created my weather structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return ( 
+  <div className="App">
+    <div className="container">
+    <Weather />
+    <footer>
+      This project was codes by Masana Mabasa and is{" "}
+      <a 
+       href="https://github.com/masana-hub/react-weather-app" 
+       target="_blank"
+      >
+       open-source on GitHub
+      </a>
+    </footer>
+    </div>
+    </div>
+    );
 }
 
